9a4c9bd9580c2528495d41349110c69d6353109d
Sohan <sohan.j12@gmail.com>
2023-08-08 15:28:42 +0530

    update url and port

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL =
-  "http://ec2-54-153-175-0.ap-southeast-2.compute.amazonaws.com";
+  "https://ec2-54-153-175-0.ap-southeast-2.compute.amazonaws.com";
 
-export const PORT = "3000";
+export const PORT = "443";
 //export const BACKEND_URL = "http://localhost";
